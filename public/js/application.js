fca2b7f20bb00bb66b2e3d2a26c38ca9037ef28d
dbc-apprentice <dbc-apprentice@devbootcamp.com>
2013-07-03 18:03:49 -0700

    finished it, and fixed spelling error on onigiri...seriously

--- public/js/application.js
+++ public/js/application.js
@@ -1,8 +1,18 @@
 $(document).ready(function() {
   $('.awesomeness_teller').on('click', function() {
     var setVal = $(this).attr('data-heading');
-    $('#awesomeness_holder').html('<h2>'+setVal+'</h2>')
+    $('#awesomeness_holder').html('<h2>'+setVal+'</h2>');
   });
+
+
+    $('.skill_teller').on('click', function(){
+      var setSkill = window.location.pathname.replace('/skills/','');
+      $('#skill_holder').text(setSkill);
+    });
+    
+   $('.meal_teller').on('click', function(){
+      $('#meal_holder').text(sampleMeal);
+    });
 
   // What other events do you need to bind to make the other pages work?
 });
