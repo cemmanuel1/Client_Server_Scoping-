fe3a82a9c0fd200172883724600390d46499da0c
dbc-apprentice <dbc-apprentice@devbootcamp.com>
2013-07-03 17:37:33 -0700

    awesome page button replacing works

--- public/js/application.js
+++ public/js/application.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   $('.awesomeness_teller').on('click', function() {
-
+    var setVal = $(this).attr('data-heading');
+    $('#awesomeness_holder').html('<h2>'+setVal+'</h2>')
   });
 
   // What other events do you need to bind to make the other pages work?
